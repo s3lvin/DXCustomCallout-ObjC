e1ac08dc12e9149c6ed1784ed5cdf83c89d85cc1
s3lvin <mariaselvin@gmail.com>
2015-04-18 11:56:36 +0530

    Added test case for checking allocation

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -6,4 +6,41 @@
 //  Copyright (c) 2014 s3lvin. All rights reserved.
 //
 
-${TEST_EXAMPLE}
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import <DXAnnotationView.h>
+#import <DXAnnotationSettings.h>
+#import <MapKit/MapKit.h>
+
+@interface DXAnnotation : NSObject <MKAnnotation>
+
+@property(nonatomic, assign) CLLocationCoordinate2D coordinate;
+
+@end
+
+@interface DXAnnotationViewTests : XCTestCase
+
+@property(nonatomic, strong) DXAnnotation *annotation;
+
+@end
+
+@implementation DXAnnotationViewTests
+
+- (void)setUp {
+    [super setUp];
+    self.annotation = [DXAnnotation new];
+    self.annotation.coordinate = CLLocationCoordinate2DMake(12.9667, 77.5667);
+}
+
+- (void)tearDown {
+    self.annotation = nil;
+    [super tearDown];
+}
+
+- (void)testAlloc {
+    // This is an example of a functional test case.
+    DXAnnotationView *annotationView = [[DXAnnotationView alloc] initWithAnnotation:self.annotation reuseIdentifier:NSStringFromClass([DXAnnotationView class]) pinView:[UIView new] calloutView:[UIView new] settings:[DXAnnotationSettings defaultSettings]];
+    XCTAssert(annotationView != nil, @"Allocation Pass");
+}
+
+@end
